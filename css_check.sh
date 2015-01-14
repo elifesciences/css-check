58bf952e786afb111026abb8c7c4b5a3dc364d38
root <root@central-logging-dev>
2015-01-14 16:05:23 +0000

    added shbang to check script

--- css_check.sh
+++ css_check.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 LOGPATH=/var/log/css_check.log
 
 /usr/bin/phantomjs css_check.js http://elifesciences.org/content/3/e04120 $LOGPATH .page-title font-family "'Avenir LT W01 85 Heavy', sans-serif" head false
